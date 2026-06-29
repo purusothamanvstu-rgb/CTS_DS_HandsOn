2621e8732367b43be28d3fbc76ec7803ecabd195
Purusothaman <purusothaman.vstu@gmail.com>
2026-06-29 18:59:29 +0530

    Complete Module 4 Exercise 1: JUnit Setup

---
+++ module_4_testing/src/test/java/com/dshandson/testing/SetupTest.java
@@ -0,0 +1,14 @@
+package com.dshandson.testing;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SetupTest {
+    
+    @Test
+    public void testJUnitSetup() {
+        // This is a simple test that will always pass, 
+        // proving JUnit is successfully installed and running.
+        assertTrue("JUnit is working correctly!", true);
+    }
+}
