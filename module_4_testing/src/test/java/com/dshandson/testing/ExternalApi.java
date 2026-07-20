d831e17d30d9cff99c42d8a454b75376da0b94c7
Purusothaman <purusothaman.vstu@gmail.com>
2026-07-20 10:16:53 +0530

    Add Java unit testing with JUnit and Maven

---
+++ module_4_testing/src/test/java/com/dshandson/testing/ExternalApi.java
@@ -0,0 +1,7 @@
+package com.dshandson.testing;
+
+public class ExternalApi {
+    public String getData() {
+        return "Real Data";
+    }
+}
---
+++ module_4_testing/src/test/java/com/dshandson/testing/SetupTest.java
@@ -0,0 +1,13 @@
+package com.dshandson.testing;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SetupTest {
+    
+    @Test
+    public void testJUnitSetup() {
+       
+        assertTrue("JUnit is working correctly!", true);
+    }
+}
